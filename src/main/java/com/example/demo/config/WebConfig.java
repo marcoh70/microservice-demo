d384e3819ec578427100fc95334b20ace7e8b716
Marco Antonio Hernandez Perez <markohp@Marcos-Mac-mini.local>
2025-10-30 13:21:11 -0600

    Adding cors urls

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -10,6 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
+                .allowedOrigins(
+                    "http://localhost:8080"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
